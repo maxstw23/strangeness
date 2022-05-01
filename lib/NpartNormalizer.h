b0ff0eef6aa6333953767f8b657eec01857727e1
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2022-04-30 22:20:18 -0700

    Correct header mistake

--- lib/NpartNormalizer.h
+++ lib/NpartNormalizer.h
@@ -6,15 +6,20 @@
 class NpartNormalizer
 {
 private:
-    static const const std::map<int, float> ratio_ox;
-    static const const std::map<int, float> ratio_oxb;
-    static const const std::map<int, float> ratio_obx;
-    static const const std::map<int, float> ratio_obxb;
+    static std::map<int, float> ratio_ox;
+    static std::map<int, float> ratio_oxb;
+    static std::map<int, float> ratio_obx;
+    static std::map<int, float> ratio_obxb;
+
+    static std::map<int, float> ratio_x;
+    static std::map<int, float> ratio_xb;
+    static std::map<int, float> ratio_o;
+    static std::map<int, float> ratio_ob;
     
 public:
     NpartNormalizer() {}
     virtual ~NpartNormalizer() {}
-    float weight(int npp, TString case);
+    float weight(int npp, TString compare_case);
 
 ClassDef(NpartNormalizer, 1)
 };
---
+++ lib/NpartNormalizer_cpp.d
@@ -0,0 +1,26 @@
+
+# DO NOT DELETE
+
+./NpartNormalizer_cpp.so: NpartNormalizer.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/TString.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/TMathBase.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/Rtypes.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/RtypesCore.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/ROOT/RConfig.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/RVersion.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/RConfigure.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/DllImport.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/snprintf.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/strlcpy.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/TGenericClassInfo.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/TSchemaHelper.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/TIsAProxy.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/TVirtualIsAProxy.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/ROOT/RStringView.hxx
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/ROOT/RWrap_libcpp_string_view.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/ROOT/libcpp_string_view.h
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/ROOT/TypeTraits.hxx
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/ROOT/RSpan.hxx
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/ROOT/span.hxx
+./NpartNormalizer_cpp.so: /mnt/d/Root/builddir/include/RVersion.h /mnt/d/Root/builddir/include/RConfig.h /mnt/d/Root/builddir/include/TClass.h /mnt/d/Root/builddir/include/TDictAttributeMap.h /mnt/d/Root/builddir/include/TInterpreter.h /mnt/d/Root/builddir/include/TROOT.h /mnt/d/Root/builddir/include/TBuffer.h /mnt/d/Root/builddir/include/TMemberInspector.h /mnt/d/Root/builddir/include/TError.h /mnt/d/Root/builddir/include/RtypesImp.h /mnt/d/Root/builddir/include/TIsAProxy.h /mnt/d/Root/builddir/include/TFileMergeInfo.h /mnt/d/Root/builddir/include/TCollectionProxyInfo.h /mnt/d/Root/builddir/bin/rootcling
+NpartNormalizer_cpp__ROOTBUILDVERSION= 6.14/02
