bee97487fa1824696c8e57ed9730beaeabc65ca9
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2022-06-22 15:58:01 -0700

    Add python script for moving bad AMPT files

---
+++ move_bad_AMPT.py
@@ -0,0 +1,5 @@
+from RCFNavigator import FileMover
+
+mover = FileMover('/star/data01/pwg/xiatong/AMPT/14GeV_New/good/')
+mover.move('/star/data01/pwg/xiatong/git/Ampt_Runner_New/14GeV_sm/output/',
+           '/star/data01/pwg/xiatong/AMPT/14GeV_New/bad/')
