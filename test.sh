bf3f0a5ec21bac821b92da9eeff146950b8c4f62
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2023-08-20 01:36:07 -0700

    starpro

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 testlist="test.list"
 jobid=$1
 mode=$2
-starver pro
-root4star -b -q check_Q.C++\(\"$testlist\",\"$jobid\",&mode;\)
+starpro
+root4star -b -q check_Q.C\(\"$testlist\",\"$jobid\",&mode;\)
