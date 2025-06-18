b305b318ddbea2de6766fa7c27b1fcc36fb6195f
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2025-06-18 14:12:07 -0700

    Update get_file_list_AMPT.sh

--- get_file_list_AMPT.sh
+++ get_file_list_AMPT.sh
@@ -2,5 +2,6 @@
 
 # save file list into a list file
 # the dir is /star/data01/pwg/xiatong/AMPT/14GeV/*root
+# to avoid argument list too long, use find
 
-ls /star/data01/pwg/xiatong/AMPT/14GeV/*root > AMPT_14GeV.list
+find /star/data01/pwg/xiatong/AMPT/14GeV/ -name "*.root" > AMPT_14GeV.list
