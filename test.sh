3c048234defe38e2913b92073e4199017272d7e3
maxstw23 <74139226+maxstw23@users.noreply.github.com>
2025-06-18 18:23:32 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
-#!/bin/tcsh
-testlist="./test.list"
-jobid=$1
-mode=$2
-starpro
+#!/bin/csh
+set estlist="./test.list"
+set jobid=$1
+set mode=$2
 root4star -b -q check_Q.C\(\"$testlist\",\"$jobid\",$mode\)
